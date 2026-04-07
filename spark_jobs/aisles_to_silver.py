9093d7614d99ad92178003be1edab376fa19b220
kevynzap <86108724+kevynzap@users.noreply.github.com>
2026-04-07 23:05:36 +0000

    criação da pasta aisles na input_data

--- spark_jobs/aisles_to_silver.py
+++ spark_jobs/aisles_to_silver.py
@@ -25,8 +25,8 @@ aisles = (
         }
     )
     .select(
-        col("id_corredor"), 
-        lower(trim(col("corredor"))).alias("corredor"),
+        #col("id_corredor"), 
+        #lower(trim(col("corredor"))).alias("corredor"),
         col("dt_carga")
     )
 )
